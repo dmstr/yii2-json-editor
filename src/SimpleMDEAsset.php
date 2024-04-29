4dbbdf2147a982f26d66d3134c7455c5ded20705
German Bisurgi <g.bisurgi@herzogkommunikation.de>
2024-04-29 15:17:47 +0200

    Added simplemde assets

---
+++ src/SimpleMDEAsset.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @link http://www.diemeisterei.de/
+ * @copyright Copyright (c) 2018 diemeisterei GmbH, Stuttgart
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace dmstr\jsoneditor;
+
+use yii\web\AssetBundle;
+use dosamigos\selectize\SelectizeAsset;
+use yii\web\JqueryAsset;
+use dosamigos\ckeditor\CKEditorAsset;
+
+class SimpleMDEAsset extends AssetBundle
+{
+    public $sourcePath = '@dmstr/jsoneditor/assets/simplemde';
+
+    public $js = [
+        'simplemde.min.js',
+    ];
+
+    public $css = [
+        'simplemde.min.css'
+    ];
+}
